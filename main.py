80869ba778d1c85eff1f824cae0c937b500c6098
Kyle <kyle.treoxer@gmail.com>
2022-02-26 16:47:45 +0700

    Finalisasi Convex Hull

--- main.py
+++ main.py
@@ -1,24 +1,74 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from random import uniform
 from sklearn import datasets
-data = datasets.load_iris() 
 
+print("\nChoose number for your dataset to Test Linear Separability Dataset")
+print("(Below chosen datasets from sklearn which has class / separable)")
+print(
+    '''
+    1. iris
+    2. wine
+    3. breast_cancer
+    '''
+)
+print("(Below chosen datasets from sklearn which is not separable (no classes))")
+print(
+    '''
+    4. boston
+    5. diabetes
+    '''
+)
+
+while(True):
+    n = int(input("Chosen data number: "))
+    if(n == 1):
+        data = datasets.load_iris() 
+        break
+    elif(n == 2):
+        data = datasets.load_wine() 
+        break
+    elif(n == 3):
+        data = datasets.load_breast_cancer() 
+        break
+    elif(n == 4):
+        data = datasets.load_boston()
+        break
+    elif(n == 5):
+        data = datasets.load_diabetes()
+        break
+    else: print("Invalid Number!")
+
+print("\nHere are the list of features of this data collection:\n")
+for i in range(len(data.feature_names)):
+    print(str(i+1) + "." +data.feature_names[i])
+first_feature = int(input("Choose first feature: "))-1
+second_feature = int(input("Choose second feature: "))-1
 df = pd.DataFrame(data.data, columns=data.feature_names)
 df['Target'] = pd.DataFrame(data.target)
 
-from scipy.spatial import ConvexHull
+from myConvexHull import ConvexHull
 plt.figure(figsize = (10, 6))
-colors = ['b','r','g']
-plt.title('Petal Width vs Petal Length')
-plt.xlabel(data.feature_names[0])
-plt.ylabel(data.feature_names[1])
-for i in range(len(data.target_names)):
-    bucket = df[df['Target'] == i]
-    bucket = bucket.iloc[:,[0,1]].values
-    hull = ConvexHull(bucket) #bagian ini diganti dengan hasil implementasi
-#ConvexHull Divide & Conquer
-    plt.scatter(bucket[:, 0], bucket[:, 1], label=data.target_names[i])
-    for simplex in hull.simplices:
-        plt.plot(bucket[simplex, 0], bucket[simplex, 1], colors[i])
+colors = ['b','r','g','c','m','y']
+plt.title(str(data.feature_names[first_feature])+" vs "+str(data.feature_names[second_feature]))
+plt.xlabel(data.feature_names[first_feature])
+plt.ylabel(data.feature_names[second_feature])
+
+try :
+    for i in range(len(data.target_names)):
+        bucket = df[df['Target'] == i]
+        bucket = bucket.iloc[:,[first_feature,second_feature]].values
+        hull = ConvexHull(bucket)
+        plt.scatter(bucket[:, 0], bucket[:, 1], label=data.target_names[i],color =colors[i%6])
+        hul_len = len(hull)
+        plt.plot([row[0] for row in hull]+[hull[0][0]], [row[1] for row in hull]+[hull[0][1]],color =colors[i%6])
+    plt.legend()
+except:
+    bucket = df
+    bucket = bucket.iloc[:,[first_feature,second_feature]].values
+    hull = ConvexHull(bucket)
+    plt.scatter(bucket[:, 0], bucket[:, 1],color =colors[0])
+    hul_len = len(hull)
+    plt.plot([row[0] for row in hull]+[hull[0][0]], [row[1] for row in hull]+[hull[0][1]],color =colors[0])
 plt.show()
--- myConvexHull.py
+++ myConvexHull.py
@@ -11,34 +11,46 @@ def ConvexHull(bucket):
             min_x = point
 
     #Divide into two set (left and right of line) => O(N)
-    left_batch = []
-    right_batch =[]
-    for point in bucket:
-        sideVal = distance(min_x,max_x,point)
-        if(sideVal > 0): #Point on the right side of line min_x--max_x
-            right_batch+=[point]
-        if(sideVal < 0): #Point on the left side of line min_x--max_x
-            left_batch+=[point]
-    result = []
-    print(left_batch)
-    print(min_x,max_x)
-    return result
+    left_batch,right_batch = divideSet(min_x,max_x,bucket)
+
+    #Conquer (Counter ClockWise to maintain order always use right set of oriented line)
+    return [min_x] + QuickHull(min_x,max_x, right_batch) + [max_x] + QuickHull(max_x,min_x,left_batch) 
+    
 
 def distance(begin_point, end_point, eval_point): #if on the right side, value > 0 ; if left side value < 0 ; else on the line => O(1)
     #Using Cross-Product on 2D 
     return ((end_point[0] - begin_point[0])*(begin_point[1]-eval_point[1]) - (end_point[1] - begin_point[1])*(begin_point[0]-eval_point[0]))
 
-def QuickHull(begin_point, end_point,bucket):
-    result = []
-    if bucket.length == 0:
-        return result
-    return
+def divideSet(begin_point,end_point,bucket):
+    left_batch = []
+    right_batch =[]
+    for point in bucket:
+        sideVal = distance(begin_point,end_point,point)
+        if(sideVal > 0): #Point on the right side of line min_x--max_x
+            right_batch+=[point]
+        if(sideVal < 0): #Point on the left side of line min_x--max_x
+            left_batch+=[point]
+    return left_batch,right_batch
 
-import pandas as pd
-from sklearn import datasets
-data = datasets.load_iris() 
-df = pd.DataFrame(data.data, columns=data.feature_names)
-df['Target'] = pd.DataFrame(data.target)
-bucket = df[df['Target'] == 0]
-bucket = bucket.iloc[:,[0,1]].values
-ConvexHull(bucket)
+def QuickHull(begin_point, end_point,bucket):
+    n_points = len(bucket)
+    if n_points == 0:
+        return []
+    elif n_points == 1:
+        return [bucket[0]]
+    else:
+        furthest_dist = 0
+        furthest_point = bucket[0]
+
+        #find furthest point from oriented line => O(N)
+        for point in bucket:
+            if (abs(distance(begin_point,end_point,point)) > furthest_dist):
+                furthest_dist = abs(distance(begin_point,end_point,point))
+                furthest_point = point
+        
+        #divide into two triangle parts ("upper left" and "upper right")
+        #Get "upper right" set
+        temp_set, upright_set = divideSet(begin_point,furthest_point,bucket)
+        #Get "upper left" set
+        temp_set, upleft_set = divideSet(furthest_point,end_point,temp_set)
+        return QuickHull(begin_point,furthest_point,upright_set) + [furthest_point] + QuickHull(furthest_point,end_point,upleft_set) 
